d515550ae8960f66952ad87c73b7a9bdf2a27fa3
David Johnston <david.johnston@flip.com.au>
2025-12-15 13:41:41 +1100

    Add fourth component

---
+++ src/FourthComponent/FourthComponent.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="fourth-component">I am the fourth component</div>
+</template>
+
+<style scoped>
+.fourth-component {
+  border: dashed 2px blue;
+
+  color: blue;
+  padding: 1em;
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as SomeComponent } from "./SomeComponent/SomeComponent.vue";
 export { default as SomeOtherComponent } from "./SomeOtherComponent/SomeOtherComponent.vue";
 export { default as Box } from "./Box/Box.vue";
+export { default as FourthComponent } from "./FourthComponent/FourthComponent.vue";
